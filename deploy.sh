df0143ce302b92ba7c112d5cdc29e6d0d38c339c
Mounir Lamouri <mounir@lamouri.fr>
2016-04-28 00:07:25 +0100

    Use 'rm -rf out/*' instead of 'rm -rf out/**/*'.
    The latter seems to not work consistently and behave as out/*/*.

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf out/**/* || exit 0
+rm -rf out/* || exit 0
 
 # Adding back published/ dir.
 cp -r published/ out/ || exit 0
